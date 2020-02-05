588064e57a15876a10b8a94e5c4de6ab7d4298b3
FrancescoVillani <Francesco.villani99@gmail.com>
2020-02-05 21:11:39 +0100

    added gyroscope

---
+++ app/src/main/java/com/example/softwareing/OrientationListener.java
@@ -0,0 +1,38 @@
+package com.example.ingegneriadelsoftware;
+
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+
+public class OrientationListener implements SensorEventListener {
+
+    private SensorManager sm;
+    private Sensor sm_sensor;
+    private final float[] sm_rotation_matrix = new float[16];
+    private float[] sm_array = new float[3];
+    private double rotation;
+
+    public OrientationListener (SensorManager sm){
+        this.sm = sm;
+        sm_sensor = sm.getDefaultSensor(Sensor.TYPE_GAME_ROTATION_VECTOR);
+        sm.registerListener(this, sm_sensor, 10000);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        SensorManager.getRotationMatrixFromVector(sm_rotation_matrix, event.values);
+        SensorManager.getOrientation(sm_rotation_matrix, sm_array);
+
+        rotation = (Math.toDegrees(sm_array[0])+360) % 360;
+    }
+
+    public double getRotation(){
+        return this.rotation;
+    }
+
+    /* non serve implementarlo */
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+    }
+}
